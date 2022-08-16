1c708caaf7991606de34e656de2d90d2a6d0fe9d
iprogrammist <110420915+iprogrammist@users.noreply.github.com>
2022-08-16 08:21:10 +0300

    Display products, step 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_15_041112) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_15_055434) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
