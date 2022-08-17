7cffb2d4e27f6785e3959ab7c25f6c5dda0eff27
iprogrammist <110420915+iprogrammist@users.noreply.github.com>
2022-08-17 08:19:33 +0300

    Update orders input field

--- public/app.js
+++ public/app.js
@@ -11,6 +11,8 @@ function something()
 	
 }
 
+
+
 function add_to_cart(id)
 {
 	var key = 'product_' + id;
@@ -19,31 +21,20 @@ function add_to_cart(id)
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
 
-	// вывод количества товаров в корзине
-	alert('Items in your cart: ' + cart_get_number_of_items());
+	update_orders_input();
 }
 
-function up
+
+
+function update_orders_input()
+{
+	var orders = cart_get_orders();
+	$('#orders_input').val(orders);
+}
+
+
 
 function cart_get_number_of_items()
-{
-	var orders = '';
-
-	for(var i = 0; i < window.localStorage.length; i++)
-	{
-		var key = window.localStorage.key(i); // получаем ключ
-		var value = window.localStorage.getItem(key); // получаем значение, аналог в ruby: hh[key] = x
-
-		if(key.indexOf('product_') == 0)
-		{
-			orders = orders + key + '=' + value + ',';
-		}	
-	}
-
-	return orders;	
-}
-
-function cart_get_orders()
 {
 	var cnt = 0;
 
@@ -59,6 +50,26 @@ function cart_get_orders()
 	}
 
 	return cnt;	
+}
+
+
+
+function cart_get_orders()
+{
+	var orders = '';
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); // получаем ключ
+		var value = window.localStorage.getItem(key); // получаем значение, аналог в ruby: hh[key] = x
+
+		if(key.indexOf('product_') == 0)
+		{
+			orders = orders + key + '=' + value + ',';
+		}	
+	}
+
+	return orders;	
 }
 
 
